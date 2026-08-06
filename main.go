7cba023f16921039f867565f84cf0282969b04b1
makismover <maksimover@tutu.ru>
2026-08-07 00:00:34 +0300

    structures
    homework

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
-import (
-	"github.com/fatih/color"
-)
+import "fmt"
+
+type Film struct {
+	Name string
+	PublicationDate int
+	Genre []string
+	Rating float64
+}
+
+type Account struct {
+	Id int
+	Owner string
+	Balance float64
+}
 
 func main() {
-	color.Red("Hexlet for Brave!")
+	film := Film{
+		Name: "The Matrix",
+		PublicationDate: 1999,
+		Genre: []string{"Action", "Sci-Fi"},
+		Rating: 8.7,
+	}
+	fmt.Println(film.Name, "(", film.PublicationDate, ") - жанры: ", film.Genre, " - рейтинг: ", film.Rating)
+
+	account := Account{
+		Id: 1,
+		Owner: "John Doe",
+		Balance: 500,
+	}
+	fmt.Println(account.Balance)
+	Deposit(&account, 500)
+	fmt.Println(account.Balance)
+}
+
+func Deposit(acc *Account, amount float64) {
+	acc.Balance += amount
 }
