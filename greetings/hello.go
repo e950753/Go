3a15dd10987d2571775baf9f3dd73eaf9822296f
makismover <maksimover@tutu.ru>
2026-08-06 21:15:28 +0300

    hello_programm
    packages in Go

---
+++ greetings/hello.go
@@ -0,0 +1,7 @@
+package greetings
+
+var greetings = "Golang for Brave!"
+
+func Hello() string {
+	return greetings
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"./greetings"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(greetings.Hello())
+}
